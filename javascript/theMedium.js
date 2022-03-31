54393ee5475f987328c4baf8fa282359531f1c5b
semsemihere <sh6042@nyu.edu>
2022-03-31 11:10:22 -0400

    FirstTry

---
+++ javascript/theMedium.js
@@ -0,0 +1,60 @@
+// console.log("the Mess age!");
+
+// // an empty array to keep track of clicks??!!!!
+// let theEmptyArray = [];
+// let theBody = document.querySelector("body");
+// const theBall = document.getElementById("ball")
+
+// theBall.addEventListener('click', magicClick)
+
+// function magicClick(){
+//     console.log("you clicked!")
+//     theBody.style.backgroundColor = "yellow";
+// }
+
+
+// const count = document.querySelector(".hits");
+// const catContainer = document.querySelector("#catContainer");
+
+// // const catContainer = document.getElementById("cat-container");
+
+// count.addEventListener('click', magicClick)
+
+// function magicClick(){
+//     // add count
+//     hits++;
+//     renderHits();
+
+//     // draw cat
+//     const newDiv = document.createElement("div");
+//     console.log("meaw");
+//     newDiv.classList.add("cat-shadow")
+//     catContainer.appendChild(newDiv);
+
+// }
+
+// function renderHits(){
+//     count.innerHTML = hits; 
+// }
+
+let hits = 0; 
+
+
+const count = document.querySelector(".count");
+const catContainer = document.getElementById("cat-container");
+
+count.addEventListener("click", AddNew);
+
+function AddNew() {
+    hits++;
+    renderHits();
+
+    const newDiv = document.createElement("div");
+    console.log("add");
+    newDiv.classList.add("div-shadow");
+    catContainer.appendChild(newDiv);
+}
+
+function renderHits(){
+    count.innerHTML = hits; 
+}
